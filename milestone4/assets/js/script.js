bc50e9a3fdb7b653ad48212267241f49e27f3a1b
AleColumn99 <alecolumn.99@gmail.com>
2021-05-12 15:30:24 +0200

    milestone 3 corretto

--- milestone4/assets/js/script.js
+++ milestone4/assets/js/script.js
@@ -151,7 +151,7 @@ const app = new Vue ({
             status: 'received'
           }
         ],
-      }/* ,
+      },
       {
         name: 'Davide',
         avatar: '_8',
@@ -173,52 +173,38 @@ const app = new Vue ({
             status: 'received'
           }
         ],
-      } */
+      }
     ],
-    contactActive: 0,
-    responses: [
-      'Assolutamente no!',
-      'Bella ideaa',
-      'Tu dici?',
-      'Ok può andare',
-      'No dai questo no',
-      'Bellissimooo',
-      'Sei un folle fra'
-    ]
+    contactActive: 0
+
+  },
+  mounted() {
 
   },
   methods: {
 
-    sendMessage() {
-      if(this.inputMessage.length > 0) {
-
-        this.pushMessage(this.inputMessage, 'sent')
-        this.inputMessage = '';
-
-        setTimeout(() => {
-          let response = this.responses[Math.floor(Math.random() * this.responses.length-1) + 1];
-          this.pushMessage(response, 'received')
-        }, 1000);
-      }
-    },
-    pushMessage(text, status) {
-      this.contacts[this.contactActive].messages.push({
-        date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-        text: text,
-        status: status
+    sendMessage(contact) {
+      contact.messages.push({
+        date: '11/05/2021 11:15:00',
+        text: this.inputMessage,
+        status: 'sent'
       });
+      this.inputMessage = '';
+      setTimeout(() => {
+        contact.messages.push({
+          date: '11/05/2021 11:15:10',
+          text: 'OK',
+          status: 'received'
+        });
+      }, 1000);
     },
-    lastAccess(index) {
-      let contactMessages = this.contacts[index].messages;
-      return contactMessages[contactMessages.length-1].date;
-    },
-    lastMessage(index) {
-      let messages = this.contacts[index].messages;
-      if(messages[messages.length-1].text.length > 30) {
-        let splicedMsg = messages[messages.length-1].text.slice(0, 30) + "...";
-        return splicedMsg;
-      } else {
-        return messages[messages.length-1].text;
+    searchContact(contacts) {
+      for(contact in contacts) {
+        if (contact.name.includes(searchContact)) {
+          contact.visible = true;
+        } else {
+          contact.visible = false;
+        }
       }
     }
 
